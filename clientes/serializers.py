01d02828085f9f813d4af7fc3ee567eeea51f683
Eduardo Yudi Nakamura <64866477+nakamura-yudi@users.noreply.github.com>
2023-07-11 09:14:31 -0300

    finish

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -6,7 +6,6 @@ class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cliente
         fields = '__all__'
-
     def validate(self, data):
         if not cpf_valido(data['cpf']):
             raise serializers.ValidationError({'cpf':"Número de CPF inválido"})
@@ -17,3 +16,4 @@ class ClienteSerializer(serializers.ModelSerializer):
         if not celular_valido(data['celular']):
             raise serializers.ValidationError({'celular':"O número de celular deve seguir este modelo: 11 91234-1234 (respeitando os espaços e traço)"})
         return data
+
